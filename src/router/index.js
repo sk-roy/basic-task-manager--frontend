4746286f23f9e4d50a0337b242f6d8881e793cd1
Sujit Kumar Roy <sujitkumarroyy@gmail.com>
2024-08-28 17:05:16 +0600

    Fix routing for authentication, create comment

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import ProfileView from '@/views/auth/ProfileView.vue'
 import LoginView from '@/views/auth/LoginView.vue'
+import ProfileView from '@/views/auth/ProfileView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TaskCreate from '../views/task/CreateView.vue'
-import TaskUpdate from '../views/task/UpdateView.vue'
 import TaskDetails from '../views/task/DetailsView.vue'
+import TaskUpdate from '../views/task/UpdateView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,39 +12,90 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: LoginView
+      component: LoginView,
+      meta: {
+        requiresAuth: false
+      }
     },
     {
       path: '/logout',
       name: 'logout',
-      component: LoginView
+      component: LoginView,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/profile',
       name: 'profile',
-      component: ProfileView
+      component: ProfileView,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/newtask',
       name: 'newtask',
-      component: TaskCreate
+      component: TaskCreate,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/tasks/:id/update',
       name: 'taskUpdate',
-      component: TaskUpdate
+      component: TaskUpdate,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/tasks/:id/details',
       name: 'taskDetails',
-      component: TaskDetails
+      component: TaskDetails,
+      meta: {
+        requiresAuth: true
+      }
     },
   ]
 })
+
+
+
+
+const getTokenFromCookie = () => {
+  const name = "auth_token=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(";");
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i].trim();
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return null;
+}
+
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const token = getTokenFromCookie();
+    if (token) {
+      next();
+    } else {
+      next('/login');
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
