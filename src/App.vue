f1b98cddd84414181d4469a3d8930256746df83b
enosis_sujit <sujit.roy@enosisbd.com>
2024-08-30 20:34:04 +0600

    Implemented task updating notification

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,63 @@
 <script>
 import { RouterLink, RouterView } from "vue-router";
 import apiClient from "./plugins/axios";
+import Pusher from "pusher-js";
+import Echo from 'laravel-echo';
 
 export default {
   name: "app",
   data() {
     return {
+      notifications: [],
+      messages: [],
       user: [],
       loggedIn: false,
+      tasks: [],
     };
   },
 
   mounted() {
     this.getUser();
+    this.subscribeToTaskChannels();
   },
 
-  methods: {
+  methods: {    
+    async subscribeToTaskChannels() {
+      try {
+        const params = new URLSearchParams();
+        params.append('sort_key', this.sortKey);
+        params.append('sort_order', this.sortOrder);
+
+        const response = await apiClient.get('/tasks');
+        this.tasks = response.data.tasks;
+        response.data.tasks.forEach(task => {
+          console.log(task.title);
+          window.Echo.channel(`task.${task.id}`)
+            .listen('.task.update', (task) => {
+              this.addNotification(task);
+            })
+            // .listen('TaskDeleted', (event) => {
+            //     console.log(`Task ${taskId} deleted:`, event);
+            //     // Handle the event
+            // });
+        });
+
+        console.log(this.notifications);
+      } catch (error) {
+        console.error("Error during getting task:", error);
+        alert("Error during getting tasks");
+      }
+    },
+
+    addNotification(task) {
+      const notification = {
+          id: task.id,
+          message: `Task "${task.title}" was updated by ${task.creator_name}.`,
+          timestamp: new Date().toLocaleString(),
+      };
+      this.notifications.push(notification); // Add the notification to the array
+    },
+
     async logout() {
       try {
         const response = await apiClient.post("/logout");
